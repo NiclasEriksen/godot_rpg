d8a94fc9defeba9dda8aaf5099e668a70ba3d1bb
Niclas Eriksen <niclaseriksen@gmx.com>
2016-09-21 19:46:10 +0200

    Added EffectModule to projectile, it can now damage enemies (if ever there was any...)

--- entities/projectiles/Fireball.gd
+++ entities/projectiles/Fireball.gd
@@ -20,13 +20,18 @@ func _fixed_process(delta):
 func set_velocity(speed):
 	vel = Vector2(speed, 0).rotated(get_rot() - PI/2)
 
-func impact():
+func impact(body):
+	get_node("Area2D").call_deferred("set_enable_monitoring", false)
 	vel = Vector2(0, 0)
 	get_node("AnimationPlayer").play("Explode")
+	if body.is_in_group("enemies"):
+		if body.get_node("StatsModule"):
+			body.get_node("StatsModule").apply_effect(get_node("EffectModule"), null)
+			print("ANAMAI!")
 
 func _on_Area2D_body_enter( body ):
 	if not body.is_in_group("players"):
-		impact()
+		impact(body)
 
 
 func _on_AnimationPlayer_finished():
