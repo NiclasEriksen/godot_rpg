3996b9e347fa0d81f1dd5f659108ec25fa1253c7
Niclas Eriksen <niclaseriksen@gmx.com>
2016-09-27 23:34:30 +0200

    Testing on change effects on hp bars.

--- components/HPBar.gd
+++ components/HPBar.gd
@@ -2,6 +2,7 @@ extends TextureProgress
 var owner = null
 onready var cam = get_tree().get_root().get_node("Game").get_node("Camera2D")
 # onready var nc = get_node("/root/notifications")
+var old_val = null
 
 func _ready():
     set_process(true)
@@ -14,5 +15,9 @@ func register(owner):
 func _process(delta):
     if owner:
         var pos = cam.get_viewport().get_canvas_transform().xform(owner.get_pos())
-        set_pos(pos - (get_size() * get_scale()) / 2 - Vector2(0, 50))
-        set_value((owner.stats.get("hp") / owner.stats.get("max_hp")) * 100.0)
+        var val = (owner.stats.get("hp") / owner.stats.get("max_hp")) * 100.0
+        set_pos(pos - (get_size() * get_scale()) / 2 - Vector2(0, 60))
+        if not old_val == val:
+            self.old_val = val
+            set_value(val)
+            get_node("AnimationPlayer").play("val_change")
